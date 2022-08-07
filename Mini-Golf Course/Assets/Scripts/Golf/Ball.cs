75d9ed84c8e114553f3d9585152d6d3e26010753
CalebRoenigk <caleb.roenigk@union.co>
2022-08-07 11:22:23 -0400

    Ball Force Line Working
    Ball Force Line now works

--- Mini-Golf Course/Assets/Scripts/Golf/Ball.cs
+++ Mini-Golf Course/Assets/Scripts/Golf/Ball.cs
@@ -20,7 +20,16 @@ namespace Golf
         public bool isSandy = false;
         public bool isGrassy = false;
 
-        [Header("Gamestate")]
+        [Header("Camera Data")]
+        private Camera mainCamera;
+        private Plane mouseProjectionPlane;
+        private Vector3 mouseWorldPosition;
+
+        [Header("Hitting")]
+        private Vector3 hittingWorldPosition;
+        [SerializeField] private float hitStrength = 1f;
+
+            [Header("Gamestate")]
         public Vector3 lastHitPosition;
         public Vector3 lastHitRotation;
         public Vector3 terrainNormal;
@@ -35,6 +44,7 @@ namespace Golf
             // Store the default components
             rigidbody = GetComponent<Rigidbody>();
             forceLine = transform.GetChild(0).GetComponent<LineRenderer>();
+            mainCamera = Camera.main;
         }
 
         private void Update()
@@ -43,16 +53,17 @@ namespace Golf
             if (isResting)
             {
                 // Get the world position of the mouse
-                Vector3 mouseWorldPosition = GetMouseWorldPosition();
+                mouseWorldPosition = GetMouseWorldPosition();
 
-                // Return if the mouse world position is actually negative infinity (the mouse position returned 'null')
-                if (float.IsNegativeInfinity(mouseWorldPosition.x))
+                // Only update if the mouse world position is not 'null' (the mouse position returns negative infinity for all values when it should be null)
+                if (!float.IsNegativeInfinity(mouseWorldPosition.x))
                 {
-                    return;
-                }
+                    // Clamp the mouse position to the hit strength and store it as the hitting world position
+                    hittingWorldPosition = ClampPointToRadius(mouseWorldPosition, transform.position, hitStrength);
 
-                // Draw the force line
-                DrawForceLine(mouseWorldPosition);
+                    // Draw the force line
+                    DrawForceLine(hittingWorldPosition);
+                }
             }
             
         }
@@ -93,11 +104,29 @@ namespace Golf
 
         private void OnDrawGizmos()
         {
-            // Draw the terrain normal when the ball is resting
+            // If the ball is resting
             if (isResting)
             {
+                // Draw the terrain normal
                 Gizmos.color = Color.cyan;
                 Gizmos.DrawRay(transform.position, terrainNormal * 0.25f);
+                
+                // Draw the mouse plane
+                Vector3 mousePlaneSize = new Vector3(2f, 0f, 2f);
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawWireCube(transform.position, mousePlaneSize);
+                Gizmos.color = new Color(0f, 1f, 1f, 0.125f);
+                Gizmos.DrawCube(transform.position, mousePlaneSize);
+                
+                // Draw mouse position
+                if (!float.IsNegativeInfinity(mouseWorldPosition.x))
+                {
+                    Gizmos.color = Color.cyan;
+                    Gizmos.DrawSphere(hittingWorldPosition, 0.05f);
+                    
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawSphere(mouseWorldPosition, 0.0625f);
+                }
             }
             
         }
@@ -127,10 +156,10 @@ namespace Golf
         // Gets the mouse from the camera view
         private Vector3 GetMouseWorldPosition()
         {
-            Plane mousePlane = new Plane(Vector3.up, 0);
+            mouseProjectionPlane = new Plane(terrainNormal, transform.position);
             float distance;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (mousePlane.Raycast(ray, out distance))
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (mouseProjectionPlane.Raycast(ray, out distance))
             {
                 return ray.GetPoint(distance);
             }
@@ -144,11 +173,28 @@ namespace Golf
         private void DrawForceLine(Vector3 worldPoint)
         {
             // Create the force line positions
-            Vector3[] forcePositions = new Vector3[] { transform.position, worldPoint };
+            Vector3[] forcePositions = new Vector3[] { worldPoint, transform.position };
             
             // Set the force line positions
             forceLine.SetPositions(forcePositions);
             forceLine.enabled = true;
+        }
+        
+        // Clamps a point to a radius given a center
+        private Vector3 ClampPointToRadius(Vector3 point, Vector3 center, float radius)
+        {
+            // Get the distance to the center point
+            float distanceToCenter = Vector3.Distance(point, center);
+ 
+            //If the distance is greater than the radius the point must be clamped
+            if (distanceToCenter > radius)
+            {
+                Vector3 fromCenterToPoint = point - center;
+                fromCenterToPoint *= radius / distanceToCenter;
+                point = center + fromCenterToPoint;
+            }
+
+            return point;
         }
     }
 }
